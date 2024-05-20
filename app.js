f0dbfb7e65b48e985975d5b0516bded5a63a2d7b
Zainab Murtaza <162459926+Zainab090@users.noreply.github.com>
2024-05-20 22:05:05 +0500

    Javascript Assignment#4

---
+++ app.js
@@ -0,0 +1,22 @@
+// //Assignment#1
+// let variable1,variable2,variable3;
+// //Assignment#2
+ let legal;     //legal variable#1
+ let Legal;     //legal variable#2
+ let legal1;    //legal variable#3
+ let $legal;    //legal variable#4
+ let _legal;    //legal variable#5
+// //Illegal variable
+ let 1Illegal;    //Illegal variable#1
+ let Ill egal;    //Illegal variable#2
+ let @illegal;    //Illegal variable#3
+ let function;    //Illegal variable#4
+ let var;         //Illegal variable#5
+//Assignment#3
+document.write("<h1>“Rules for naming JS variables”</h1><br>");
+document.write("Variable names can only contain,_ , numbers and $. For example : $my_1stVariable <br>");
+document.write("Variable must begin with a letter, $ or _ . For example : $name, _name or name<br>");
+document.write("Variable names are case sensitive <br>");
+document.write("Variable names should not be JS keywords or reserved words");
+//End
+
